155350f837c8be8ab1d9f7fad1a9e5e3d7bbe847
jihazard <yoonjh238@gmail.com>
2019-04-30 14:07:55 +0900

    URL Routing 처리

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ var app = express()
 app.listen(3000,function(){
     console.log("start express server on port 3000")
 })
+//URL루트 설정하기
+app.get('/',function(req,res){
+ 
+    res.sendFile(__dirname+"/public/main.html")
+})
+
