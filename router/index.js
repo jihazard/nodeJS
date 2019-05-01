2a1fcc2b9cfd835b43d389b2220c5e543cffbfd6
jihazard <yoonjh238@gmail.com>
2019-05-01 21:03:25 +0900

    데이터 입력

--- router/index.js
+++ router/index.js
@@ -5,12 +5,12 @@ var router = express.Router();
 var path = require(`path`);
 var main = require('./main')
 var email = require('./email')
+var join = require('./join')
 
 
-
-
-router.use("/",main)
+router.use(main)
 router.use("/email",email)
+router.use("/join",join)
 
 
 module.exports=router
---
+++ router/join.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var app = express()
+var router = express.Router();
+
+var path = require(`path`);
+
+var mysql = require(`mysql`)
+var connection = mysql.createConnection({
+    host:"localhost",
+    port :3306,
+    user : "hyb",
+    password:"hyb01",
+    database:"com"
+
+})
+
+connection.connect()
+
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,"../public/join.html"))
+})
+
+
+router.post('/create',function(req,res){
+
+    var email = req.body.email;
+    var name = req.body.name;
+    var pw = req.body.pw;
+    console.log(`${name},${email},${pw}`)
+    var responseData= {}
+    /*var query = connection.query(`
+    insert into user (name,email,pw) values(${name},${email},${pw})
+    ` ,function(err,rows){
+    */
+
+        var sql = {name:name,
+                   email:email,
+                   pw:pw}
+        var query = connection.query(`
+        insert into user set ?` ,sql ,function(err,rows){
+   
+    if(err) throw err
+         var data = JSON.stringify(rows)
+         console.log(rows.insertId +"/ insert success ! /" + name )
+         /*if(rows.length >= 1){
+             responseData.result ="ok"
+             responseData.vo=rows
+         }else {
+             responseData.result ="none"
+             responseData.vo="none"
+         }*/
+
+        res.render("welcome.ejs", {"info":sql , "result":rows})
+     }) 
+ })
+
+module.exports = router
